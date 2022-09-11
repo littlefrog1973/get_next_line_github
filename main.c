33ec9d67c0eda2cb2243ed807d1576d4b648d2f8
Sukit Deeyiengyang <kitdee@hotmail.com>
2022-09-11 23:05:58 +0700

    Modified main to solve leak problem

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sdeeyien <sukitd@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 05:07:41 by sdeeyien          #+#    #+#             */
-/*   Updated: 2022/09/04 23:58:37 by sdeeyien         ###   ########.fr       */
+/*   Updated: 2022/09/11 22:50:50 by sdeeyien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,9 @@ int	main(int argc, char *argv[])
 		++i;
 		line = get_next_line(fd);
 		printf("In main (%d): %s", i, line);
-//		printf("In main : call %d times\n", i);
+		free(line);
 	}
 	fd = close(fd);
-	free(line);
+//	free(line);
 	return (0);
 }
