75882ab803b51297187e4bcb0d613e15b4c6dfbf
Sukit Deeyiengyang <kitdee@hotmail.com>
2022-09-27 14:10:08 +0700

    Reverting to 6e00fd8

--- get_next_line.c
+++ get_next_line.c
@@ -15,28 +15,6 @@
 #include <unistd.h>
 #include "get_next_line.h"
 
-
-char	*s_extend(char *line)
-{
-	char	*new_line;
-	size_t	s_len;
-	size_t	i;
-
-	s_len = ft_strlen(line);
-	new_line = (char *) malloc(s_len + BUFFER_SIZE + 1);
-	if (new_line == NULL)
-	{
-		free(line);
-		return (NULL);
-	}
-	i = 0;
-	while (i < s_len + BUFFER_SIZE + 1)
-		new_line[i++] = '\0';
-	ft_strlcpy(new_line, line, s_len + 1);
-	free(line);
-//	printf("In s_extend: after malloc\n");
-	return (new_line);
-}
 static char	*init_line(int fd, char *line)
 {
 	if (fd < 0)
@@ -49,6 +27,7 @@ static char	*init_line(int fd, char *line)
 		line = (char *) malloc((BUFFER_SIZE + 1) * sizeof(char));
 		if (!line)
 			return (NULL);
+//		ft_bzero(line, BUFFER_SIZE + 1);
 		line[0] = '\0';
 	}
 	return (line);
@@ -59,9 +38,9 @@ static ssize_t	check_new_line(char *buffer)
 	ssize_t	i;
 
 	i = 0;
-	while (buffer[i] != '\n' && buffer[i])
+	while (*(buffer + i) != '\n' && *(buffer + i))
 		i++;
-	if (buffer[i] == '\n')
+	if (*(buffer + i) == '\n')
 		return (i);
 	else
 		return (-1);
@@ -91,11 +70,10 @@ static char	*chop(char *line)
 		line[i] = line[j + 1 + i];
 		i++;
 	}
-	while (i < s_len)
-		line[i++] = '\0';
+	line[i] = '\0';
 	return (chop_line);
 }
-/*
+
 static char	*join_line_buffer(char *line, char *buffer, ssize_t j)
 {
 	char	*temp;
@@ -111,50 +89,45 @@ static char	*join_line_buffer(char *line, char *buffer, ssize_t j)
 	free(temp);
 	return (line);
 }
-*/
+
 char	*get_next_line(int fd)
 {
 	static char	*line;
-//	char		*buffer;
+	char		*buffer;
 	ssize_t		j;
 
-	if (fd < 0 || (line == NULL))
+	if (fd < 0 || !line)
 		line = init_line(fd, line);
 	if (!line)
 		return (NULL);
 	while (1)
 	{
-/*		buffer = (char *) malloc(BUFFER_SIZE + 1);
+		buffer = (char *) malloc(BUFFER_SIZE + 1);
 		if (!buffer)
 		{
 			free(line);
 			return (NULL);
 		}
-*/
-//		j = read(fd, buffer, BUFFER_SIZE);
-		j = read(fd, (line + ft_strlen(line)), BUFFER_SIZE);
+//		ft_bzero(buffer, BUFFER_SIZE + 1);
+		j = read(fd, buffer, BUFFER_SIZE);
 		if (j > 0)
 		{
-/*			line = join_line_buffer(line, buffer, j);
+			line = join_line_buffer(line, buffer, j);
 			if (!line)
 			{
 				free(buffer);
 				return (NULL);
 			}
-*/
 			if (check_new_line(line) >= 0)
 				break ;
-//			free(buffer);
-			line = s_extend(line);
-			if (line == NULL)
-				return (NULL);
+			free(buffer);
 			continue ;
 		}
 		else if (j < 0 || !ft_strlen(line))
 		{
 
 			free(line);
-//			free(buffer);
+			free(buffer);
 			line = NULL;
 			return (NULL);
 		}
@@ -162,6 +135,6 @@ char	*get_next_line(int fd)
 			break ;
 	}
 
-//	free(buffer);
+	free(buffer);
 	return (chop(line));
 }
--- main.c
+++ main.c
@@ -21,29 +21,28 @@ int	main(int argc, char *argv[])
 	int	fd;
 	int	i;
 	char	*line;
-//	char	*t_line;
 	double time_spent = 0.0;
 
+//	(void) argv[1];
 	clock_t	begin = clock();
 	(void) argc;
 	fd = open(argv[1], O_RDONLY);
+//	printf("main: fd = %i\n", fd);
 	line = "";
-/*	t_line = (char *) malloc(6);
-	ft_strlcpy(t_line, "01234", 6);
-	printf("t_line = %s\n", t_line);
-	t_line = s_extend(t_line);
-	ft_strlcpy(t_line, "0123456789abcde", 16);
-	printf("t_line after extended = %s\n", t_line);
-*/
 	i = 0;
 	while (line != NULL)
 	{
 		++i;
 		line = get_next_line(fd);
 		printf("In main (%d): %s", i, line);
+//		line = get_next_line(fd);
+//		printf("In main (%d): %s", i, line);
+//		line = get_next_line(fd);
+//		printf("In main (%d): %s", i, line);
 		free(line);
 	}
 	fd = close(fd);
+//	free(line);
 	clock_t end = clock();
 	time_spent += (double) (end - begin) / CLOCKS_PER_SEC;
 	printf("\ntime spent = %f\n", time_spent);
